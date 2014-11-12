f48423593291f8bf59be01eb174ec518b27cd28d
parkjeabum <herbhand@naver.com>
2014-11-12 16:17:48 +0900

    YES

--- sum/src/sumModel/member/WomanTypeInfoDto.java
+++ sum/src/sumModel/member/WomanTypeInfoDto.java
@@ -1,5 +1,75 @@
 package sumModel.member;
 
-public class WomanTypeInfoDto {
-
+public class WomanTypeInfoDto { //여자이상형정보
+	private String w_id; //여자아이디
+	private String w_type_job; //여자이상형직업
+	private int w_type_height; //여자이상형키
+	private String interest; //여자이상형관심사
+	private int w_type_weight; //여자이상형몸무게
+	private String w_type_eye; //여자이상형눈
+	private String w_type_nose; //여자이상형코
+	private String w_type_mouth; //여자이상형입
+	private String w_type_skin; //여자이상형피부색
+	private String w_type_char; //여자이상형성격
+	public String getW_id() {
+		return w_id;
+	}
+	public void setW_id(String w_id) {
+		this.w_id = w_id;
+	}
+	public String getW_type_job() {
+		return w_type_job;
+	}
+	public void setW_type_job(String w_type_job) {
+		this.w_type_job = w_type_job;
+	}
+	public int getW_type_height() {
+		return w_type_height;
+	}
+	public void setW_type_height(int w_type_height) {
+		this.w_type_height = w_type_height;
+	}
+	public String getInterest() {
+		return interest;
+	}
+	public void setInterest(String interest) {
+		this.interest = interest;
+	}
+	public int getW_type_weight() {
+		return w_type_weight;
+	}
+	public void setW_type_weight(int w_type_weight) {
+		this.w_type_weight = w_type_weight;
+	}
+	public String getW_type_eye() {
+		return w_type_eye;
+	}
+	public void setW_type_eye(String w_type_eye) {
+		this.w_type_eye = w_type_eye;
+	}
+	public String getW_type_nose() {
+		return w_type_nose;
+	}
+	public void setW_type_nose(String w_type_nose) {
+		this.w_type_nose = w_type_nose;
+	}
+	public String getW_type_mouth() {
+		return w_type_mouth;
+	}
+	public void setW_type_mouth(String w_type_mouth) {
+		this.w_type_mouth = w_type_mouth;
+	}
+	public String getW_type_skin() {
+		return w_type_skin;
+	}
+	public void setW_type_skin(String w_type_skin) {
+		this.w_type_skin = w_type_skin;
+	}
+	public String getW_type_char() {
+		return w_type_char;
+	}
+	public void setW_type_char(String w_type_char) {
+		this.w_type_char = w_type_char;
+	}
+	
 }
