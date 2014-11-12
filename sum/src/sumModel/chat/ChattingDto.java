809fe83814bd5697be5c583c493e791145f839c4
rajongwon <rajongwon@gmail.com>
2014-11-12 16:12:14 +0900

    ManBlockDto WomanBlockDto ChattingDto 입력

--- sum/src/sumModel/chat/ChattingDto.java
+++ sum/src/sumModel/chat/ChattingDto.java
@@ -1,5 +1,49 @@
 package sumModel.chat;
 
-public class ChattingDto {
+import java.util.Date;
 
+public class ChattingDto {
+	private String w_id, m_id, chat_content; // 여자ID, 남자ID, 채팅내용
+	private int chat_num; // 채팅번호
+	private Date chat_date; // 채팅일시
+
+	public String getW_id() {
+		return w_id;
+	}
+
+	public void setW_id(String w_id) {
+		this.w_id = w_id;
+	}
+
+	public String getM_id() {
+		return m_id;
+	}
+
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+
+	public String getChat_content() {
+		return chat_content;
+	}
+
+	public void setChat_content(String chat_content) {
+		this.chat_content = chat_content;
+	}
+
+	public int getChat_num() {
+		return chat_num;
+	}
+
+	public void setChat_num(int chat_num) {
+		this.chat_num = chat_num;
+	}
+
+	public Date getChat_date() {
+		return chat_date;
+	}
+
+	public void setChat_date(Date chat_date) {
+		this.chat_date = chat_date;
+	}
 }
